e8a66e3c637509828a57fe7d4c58d304142fb4fe
Lucas Chitolina <lucas.chitolina01@gmail.com>
2021-04-21 20:30:07 -0300

    first podcastr commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import {GetStaticProps} from 'next';
+
+interface Episode {
+    id: string;
+    title: string;
+    members: string;
+    // ...
+}
+
+interface HomeProps {
+  episodes: Array<Episode>
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
